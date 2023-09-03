84bb745b6d0c2271e445f0d85d43b9fc0ff42ca0
ryuu-cell <113927980+ryuu-cell@users.noreply.github.com>
2023-09-03 12:12:33 +0700

    add

--- JavaDasar1/src/Java1.java
+++
@@ -1,6 +0,0 @@
-public class Java1 {
-    public static void main(String[] args) {
-        
-    }
-    
-}
--- JavaDasar1/src/com/javadasar/Main.java
+++ JavaDasar1/src/com/javadasar/Main.java
@@ -1,6 +1,8 @@
+package com.javadasar;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("keren bingitsss");
+        System.out.println("Keren bingitsss");
     }
     
 }
